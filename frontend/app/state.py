05eb71506bbe954a9e683eb3dd23ae937decd17b
DiogoCarapito <diogo.carapito@icloud.com>
2025-11-18 22:50:01 +0000

    testing new backend call

--- frontend/app/state.py
+++ frontend/app/state.py
@@ -13,8 +13,9 @@ class AppState(State):
         if self.message_input:
             # Send a POST request to the backend with a timeout
             requests.post(
-                #"http://localhost:8001/api/messages",
-                "http://backend-svc:8001/api/messages",
+                # "http://localhost:8001/api/messages",
+                # "http://backend-svc:8001/api/messages",
+                "https://test.mgfhub.com/api/messages",
                 json={"text": self.message_input},
                 timeout=5,
             )
@@ -24,8 +25,9 @@ class AppState(State):
     def load_messages(self):
         try:
             # Send a GET request to the backend with a timeout
-            #response = requests.get("http://localhost:8001/api/messages", timeout=5)
-            response = requests.get("http://backend-svc:8001/api/messages", timeout=5)
+            # response = requests.get("http://localhost:8001/api/messages", timeout=5)
+            # response = requests.get("http://backend-svc:8001/api/messages", timeout=5)
+            response = requests.get("https://test.mgfhub.com/api/messages", timeout=5)
             if response.status_code == 200:
                 messages = response.json()
                 self.messages_list = [f"{msg[0]}: {msg[1]}" for msg in messages]
