2de8a34271061d7eb5c1aacb1418092278105892
DiogoCarapito <diogo.carapito@icloud.com>
2025-11-19 02:41:10 +0000

    further backend test

--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="app",
-    backend_url="http://backend-svc:8001",
+    api_url="http://backend-svc:8001",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
